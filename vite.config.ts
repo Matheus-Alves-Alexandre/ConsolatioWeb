96d3f363f2a97cdd68e6cd03a6eb14267f33a2d0
Matheus Pc Principal <m.alexandre016@gmail.com>
2025-02-05 17:16:33 -0300

    robots

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["lucide-react"],
   },
-  publicDir: 'public', 
 });
